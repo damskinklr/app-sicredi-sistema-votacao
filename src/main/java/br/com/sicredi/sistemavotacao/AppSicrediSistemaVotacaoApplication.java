ace6afa22203c423f2bfbc58b4bf5c544f96ea56
damskinklr <douglasmd@msn.com>
2025-01-18 10:33:20 -0300

    create project

---
+++ src/main/java/br/com/sicredi/sistemavotacao/AppSicrediSistemaVotacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.sistemavotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSicrediSistemaVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSicrediSistemaVotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sicredi/sistemavotacao/AppSicrediSistemaVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.sistemavotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSicrediSistemaVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
